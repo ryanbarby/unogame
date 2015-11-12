753675e5f6e5e45b51b916b6ab6484cf901c7b14
ryanbarby <rbarby333@gmail.com>
2015-11-12 10:56:36 -0600

    Heading

---
+++ uno.py
@@ -0,0 +1,19 @@
+class Display():
+	def image():
+		print("	8 8888      88 b.             8     ,o888888o.     ")
+		print("	8 8888      88 888o.          8  . 8888     `88.   ")
+		print("	8 8888      88 Y88888o.       8 ,8 8888       `8b  ")
+		print("	8 8888      88 .`Y888888o.    8 88 8888        `8b ")
+		print("	8 8888      88 8o. `Y888888o. 8 88 8888         88 ")
+		print("	8 8888      88 8`Y8o. `Y88888o8 88 8888         88 ")
+		print("	8 8888      88 8   `Y8o. `Y8888 88 8888        ,8P ")
+		print("	` 8888     ,8P 8      `Y8o. `Y8 `8 8888       ,8P  ")
+		print("	  8888   ,d8P  8         `Y8o.`  ` 8888     ,88'   ")
+		print("	   `Y88888P'   8            `Yo     `8888888P'     ")
+					
+		
+	def rules():
+		print("Rules:")
+		print("To begin with you can have anywhere from 2-4 players.")
+		print("Each of you will get dealt 7 cards.")
+		print("There will be ")
